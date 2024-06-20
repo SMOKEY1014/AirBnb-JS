ffb2999c0b2c07e9defb3a6bb750ee82de961bd1
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-06-20 12:24:20 +0200

    fixed few stuff

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className='footer'><p>&copy; 2022 AirBnB Clone! no rights reserved - this is a demo</p>
+        <div className='footer'><p>&copy; 2024 AirBnB Clone! no rights reserved - this is a demo</p>
         <p>Privacy . Terms . Sitemap . Company Details</p></div>
   )
 }
